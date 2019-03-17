a5199691feeb41a68342e0451497a1840cf5d35f
emile robert <emile.robert123@gmail.com>
2019-03-17 01:43:49 +0000

    why does test fail here but not in terminal

--- main.py
+++ main.py
@@ -2,28 +2,47 @@ from models2 import *
 from find_website import find_domain
 from newspaper import Article as NA
 
-class Site():
-    session = sessionmaker()
-    var = session.query(Website).first()
-    print(var)
-    def __init__(self, name = name):
-        self.name = name
-        return self.name
 
-    def add_article(self, url):
-        A = NA(url)
-        A.download()
-        A.parse()
-        A.nlp()
-        self.update(text = A.text, author = A.authors, description = A.summary, title = A.title)
+def add_site():
+    session = connect()
+    # W = Website(name = 'test3')
+    # session.add(W)
+    # session.commit()
+    w1 = session.query(Website).all()
+    for w in w1:
+        print(w.name)
+
+
+class Site(Website):
+    def show(self):
+        print('1')
+        print(Website)
+        print(Website.title)
+        # self.name = name
+        # return self.name
+
+    # def add_article(self, url):
+    #     A = NA(url)
+    #     A.download()
+    #     A.parse()
+    #     A.nlp()
+    #     self.update(text = A.text, author = A.authors, description = A.summary, title = A.title)
  
-        print(Article.get(1))
-        # Website = Website.get(self.id)
+    #     print(Article.get(1))
+    #     # Website = Website.get(self.id)
         # print('ok')
 
 
 
-def main(url):
-    domain = find_domain(url)
-    site = Site(url = url)
-    site.add_article(url)
+# def main(url):
+#     session = connect()
+#     W = session.query(Website).first()
+#     W.add_article(url)
+
+def test():
+    session = connect()
+    W = session.query(Website).first()
+    Site(W).show()
+
+if __name__ == "__main__":
+    test()
--- models2.py
+++ models2.py
@@ -1,18 +1,26 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, Session, sessionmaker
+from sqlalchemy.orm import relationship, sessionmaker
 
 Base = declarative_base()
 
 class Website(Base):
+    __tablename__ = 'website'
+
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
+    title = Column(String(50), unique=True)
     description = Column(String(50))
     leaning = Column(String(50))
     articles = relationship('Article')
-    
-    def __init__(self, name = None):
-        self.name = name
+
+    def add_article(self, url):
+        session = connect()
+        A = Article(url = url, website_id = self.id)
+        session.add(A)
+        session.commit()
+        print(A)
+        return A
+
 
     # def __init__(self): #does this work 
     #     find_stuff()
@@ -20,11 +28,28 @@ class Website(Base):
 
 
 class Article(Base):
+    __tablename__ = 'article'
+
     id = Column(Integer, primary_key=True)
     title = Column(String(50))
     description = Column(String(50))
     tag = Column(String(50))
     text = Column(String(50))
-    url = Column(String(50), unique=True)
+    url = Column(String(50), unique=True, nullable = False)
     website_id = Column(Integer, ForeignKey('website.id'))
- 
+
+
+def setup():
+    engine = create_engine('sqlite:///foorealtho.db')
+    Session = sessionmaker(bind=engine)
+    Session.configure(bind=engine)
+    Base.metadata.create_all(engine)
+    print('ok')
+
+
+def connect():
+    engine = create_engine('sqlite:///foorealtho.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
+
